b16c5ad6270ef87cc476ac31bd60e35dc2219b14
jthang <jensthang@gmail.com>
2015-01-25 20:43:23 +0800

    updated

---
+++ bookstore/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import os.path
+
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+            autoescape=None
+            )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            "index.html",
+            page_title = "Jens Books | Home",
+            header_text = "Welcome to Jens' Books!",
+            footer_text = "For more information, please email us at <a href=\"mailto:contact@jensbooks.com\">contact@jensbooks.com</a>.",
+        )
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+     main()
