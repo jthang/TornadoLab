50c682454d9657f1509a43a131cf4a8af92264e1
jthang <jensthang@gmail.com>
2015-01-27 00:29:00 +0800

    added edit function

--- bookstore_db/books_rwdb.py
+++ bookstore_db/books_rwdb.py
@@ -40,4 +40,69 @@ class MainHandler(tornado.web.RequestHandler):
             header_text = "Welcome to Burt's Books!",
         )
 
-class BookEditHandler(to)
+class BookEditHandler(tornado.web.RequestHandler):
+    def get(self, isbn=None):
+        book = dict()
+        if isbn:
+            coll = self.application.db.books
+            book = coll.find_one({"isbn": isbn})
+        self.render("book_edit.html",
+            page_title="Burt's Books",
+            header_text="Edit book",
+            book=book)
+
+    def post(self, isbn=None):
+        import time
+        book_fields = ['isbn', 'title', 'subtitle', 'image', 'author',
+            'date_released', 'description']
+        coll = self.application.db.books
+        book = dict()
+        if isbn:
+            book = coll.find_one({"isbn": isbn})
+        for key in book_fields:
+            book[key] = self.get_argument(key, None)
+
+        if isbn:
+            coll.save(book)
+        else:
+            book['date_added'] = int(time.time())
+            coll.insert(book)
+        self.redirect("/recommended/")
+
+class RecommendedHandler(tornado.web.RequestHandler):
+    def get(self):
+        coll = self.application.db.books
+        books = coll.find()
+        self.render(
+            "recommended.html",
+            page_title = "Burt's Books | Recommended Reading",
+            header_text = "Recommended Reading",
+            books = books
+        )
+
+
+class BookModule(tornado.web.UIModule):
+    def render(self, book):
+        return self.render_string(
+            "modules/book.html",
+            book=book,
+        )
+
+    def css_files(self):
+        return "/static/css/recommended.css"
+
+    def javascript_files(self):
+        return "/static/js/recommended.js"
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
+
+
